1042cf928fe2da6a57703598f3ff854f422416d8
Chris Jangl <chris@digitallycultured.com>
2023-05-28 12:06:10 -0400

    License

--- membership.php
+++ membership.php
@@ -1,11 +1,14 @@
 <?php
 /**
  * Plugin Name: Membership Management
- * Description: Manage your organization's members
+ * Description: Manage your organization's membership.
  * Author: Digitally Cultured
  * Author URI: https://digitallycultured.com/
  * Version: 0.0.1
+ * License: GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
+
 
 /**
  * Member
---
+++ readme.txt
@@ -0,0 +1,10 @@
+=== Membership Management ===
+Contributors: digitally-cultured
+Tags: membership management, CRM
+Tested up to: 6.2
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+Manage your organization or club's membership online.
+
+=== Description ===
